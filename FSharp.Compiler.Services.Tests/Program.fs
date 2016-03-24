8a142ec553a38ea6d7852131040299102bea2652
Kouji Matsui <k@kekyo.net>
2016-03-25 08:59:28 +0900

    Testing

--- FSharp.Compiler.Services.Tests/Program.fs
+++ FSharp.Compiler.Services.Tests/Program.fs
@@ -10,7 +10,7 @@ module Hoge =
     [<EntryPoint>]
     let main argv = 
 
-        let parseAndTypeCheckFSProj (fsprojPath) =
+        let parseFromFsproj (fsprojPath) =
             async {
                 // インタラクティブチェッカーのインスタンスを作成
                 let checker = FSharpChecker.Create()
@@ -19,17 +19,19 @@ module Hoge =
                 let projOptions =
                     ProjectCracker.GetProjectOptionsFromProjectFile(fsprojPath)
 
-                return! checker.ParseAndCheckProject(projOptions) 
+                let path0 = projOptions.OtherOptions.[22]
+
+                return! checker.ParseFileInProject(path0, File.ReadAllText(path0), projOptions) 
             }
 
         //let fsprojPath = "D:\\PROJECT\\Persimmon\\tests\\Persimmon.Script.Tests\\Persimmon.Script.Tests.fsproj"
-        let fsprojPath = "D:\\PROJECT\\FSharp.Compiler.Services.Tests\\FSharp.Compiler.Services.Tests\\FSharp.Compiler.Services.Tests.fsproj"
+        //let fsprojPath = "D:\\PROJECT\\FSharp.Compiler.Services.Tests\\FSharp.Compiler.Services.Tests\\FSharp.Compiler.Services.Tests.fsproj"
         let fsprojPath = "D:\\PROJECT\\Persimmon\examples\\Persimmon.Sample\\Persimmon.Sample.fsproj"
 
-        let results = parseAndTypeCheckFSProj(fsprojPath) |> Async.RunSynchronously
-        let r2 = results.GetAllUsesOfAllSymbols() |> Async.RunSynchronously
-        let re = r2 |> Seq.filter (fun symbolUse -> symbolUse.IsFromDefinition) |> Seq.map (fun symbolUse -> symbolUse.Symbol.FullName, symbolUse.Symbol) |> Seq.toArray
-        let r17ref = results.GetUsesOfSymbol(re.[17] |> snd) |> Async.RunSynchronously
-        let r70ref = results.GetUsesOfSymbol(r2.[70].Symbol) |> Async.RunSynchronously
+        let results = parseFromFsproj(fsprojPath) |> Async.RunSynchronously
+//        let r2 = results.GetAllUsesOfAllSymbols() |> Async.RunSynchronously
+//        let re = r2 |> Seq.filter (fun symbolUse -> symbolUse.IsFromDefinition) |> Seq.map (fun symbolUse -> symbolUse.Symbol.FullName, symbolUse.Symbol) |> Seq.toArray
+//        let r17ref = results.GetUsesOfSymbol(re.[17] |> snd) |> Async.RunSynchronously
+//        let r70ref = results.GetUsesOfSymbol(r2.[70].Symbol) |> Async.RunSynchronously
 
         0 // 整数の終了コードを返します
